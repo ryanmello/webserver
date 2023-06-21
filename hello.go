6e99c2f5659b774b52c2cc56dde7f0e39402e541
Ryan Mello <ryanmello897@gmail.com>
2023-06-21 11:01:13 -1000

    conditonally check the age of the user

--- hello.go
+++ hello.go
@@ -8,20 +8,19 @@ func main() {
 	fmt.Printf("Enter your name: ")
 	var name string
 	fmt.Scan(&name)
-	fmt.Printf("Hello %v", name)
-	
-	fmt.Println()
+	fmt.Printf("Hello %v \n", name)
 
 	fmt.Printf("Enter your age: ")
 	var age uint
 	fmt.Scan(&age)
-	fmt.Printf("You are %v years old", age)
+	fmt.Printf("You are %v years old \n", age)
 
-	fmt.Println()
+	fmt.Println(age >= 10)
 
-	fmt.Printf("What is your favorite color? ")
-	var color string
-	fmt.Scan(&color)
-	fmt.Printf("Youre favorite color is %v", color)
+	if age >= 10 {
+		fmt.Println("You can play the game")
+	} else {
+		fmt.Println("You can't play the game")
+	}
 
 }
