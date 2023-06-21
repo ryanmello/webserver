45aa99f25b88b22ff6e6e0628b66f470c74c37e9
Ryan Mello <ryanmello897@gmail.com>
2023-06-20 15:49:14 -1000

    read user's age

--- hello.go
+++ hello.go
@@ -5,7 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to my game")
 
+	fmt.Printf("Enter your name: ")
 	var name string
 	fmt.Scan(&name)
-	fmt.Println(name)
+	fmt.Printf("Hello %v", name)
+	
+	fmt.Println()
+
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+	fmt.Printf("You are %v years old", age)
 }
