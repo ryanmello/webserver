f96e7466ab1e610994a1e35df9ddaa6fd547c9ff
Ryan Mello <ryanmello897@gmail.com>
2023-06-20 15:29:24 -1000

    read useutr inp

--- hello.go
+++ hello.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-func anotherFunction(num string){
-	fmt.Println(num)
-}
-
 func main() {
-	anotherFunction("15")
-	fmt.Println("Hello")
+	fmt.Println("Welcome to my game")
+
+	var name string
+	fmt.Scan(&name)
+	fmt.Println(name)
 }
