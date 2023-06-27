7659f1c3a7e3c9e26401c9e44e37de130274a95f
Ryan Mello <ryanmello897@gmail.com>
2023-06-27 13:05:56 -1000

    welcome endpoint

--- hello.go
+++ hello.go
@@ -33,11 +33,26 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
+func welcomeHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/welcome" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Methot Not Supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "WELCOME!")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/welcome", welcomeHandler)
 
 	fmt.Printf("starting server at port 8080 \n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
