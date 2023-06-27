be20641048f0ade5598bb746a4d135c1ab08b692
Ryan Mello <ryanmello897@gmail.com>
2023-06-26 15:06:37 -1000

    hello handler method

--- hello.go
+++ hello.go
@@ -6,14 +6,28 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "HELLO!")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Printf("starting server at port 8090 \n")
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	fmt.Printf("starting server at port 8080 \n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
